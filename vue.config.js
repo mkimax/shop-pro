0efff4c746a1055f19bd9a29f0f6e5a56031e993
mk max <chapai2022@163.com>
2023-04-10 19:02:50 +0800

    feat(增加了element-plus和windcss): 增加了element-plus和windcss

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,29 @@
 const { defineConfig } = require('@vue/cli-service')
+const WindiCSSWebpackPlugin = require('windicss-webpack-plugin')
+const AutoImport = require('unplugin-auto-import/webpack')
+const Components = require('unplugin-vue-components/webpack')
+const { ElementPlusResolver } = require('unplugin-vue-components/resolvers')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  // 配置打包后静态资源路径,解决白屏问题
+  publicPath: './',
+  devServer: {
+    open: true,
+    port: 8080,
+    host: 'localhost',
+    https: false
+  },
+  configureWebpack: {
+    plugins: [
+      AutoImport({
+        resolvers: [ElementPlusResolver()]
+      }),
+      Components({
+        resolvers: [ElementPlusResolver()]
+      }),
+      new WindiCSSWebpackPlugin()
+    ]
+  },
+  // 关闭eslint
+  lintOnSave: false
 })
