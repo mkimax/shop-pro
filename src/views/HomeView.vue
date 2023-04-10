cda28c46179179b7e1ed652695fa1d447a99d3fe
mk max <chapai2022@163.com>
2023-04-10 19:04:05 +0800

    feat(global): 更新了windcss

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="home bg-red-400" style="width: 100px; height: 100px;">123</div>
+  <div class="home bg-red-400" style="width: 100px; height: 100px">123</div>
 </template>
 
 <script lang="ts">
